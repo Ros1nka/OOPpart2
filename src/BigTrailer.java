81bc2ab97ab92083c9370b6c2ba0c263cdc2ef31
Евгений Страхов <goodwin.s@mail.ru>
2024-08-31 18:40:44 +0300

    рц

---
+++ src/BigTrailer.java
@@ -0,0 +1,6 @@
+public class BigTrailer implements Trailer{
+    @Override
+    public void check() {
+        System.out.println("Проверка большого трейлера");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        final Bicycle b1 = new Bicycle("b1", 2);
+//        final Bicycle b2 = new Bicycle("b2", 2);
+        final Car c2 = new Car("c2", 4);
+//        final Car c1 = new Car("c1", 4);
+        final Truck t1 = new Truck("t1", 8, new SmallTrailer());
+        final Truck t2 = new Truck("t2", 8, new BigTrailer());
+//        final Truck t3 = new Truck("t3", 8, c2);
+
+        final Checkable[] wheeledTransports = {t2, t1, new BigTrailer()};
+        for (Checkable wheeledTransport : wheeledTransports) {
+            wheeledTransport.check();
+        }
     }
 }
